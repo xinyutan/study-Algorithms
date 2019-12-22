2e7f9dff37210e7b335b7e84e5c03b35915af971
xinyutan <xinyutan@umich.edu>
2019-12-22 17:32:29 -0500

    ex1 evacuation finished

--- coursera_AdvAlgorithmsAndComplexity/Programming-Assignment-1/evacuation/evacuation.py
+++ coursera_AdvAlgorithmsAndComplexity/Programming-Assignment-1/evacuation/evacuation.py
@@ -1,4 +1,6 @@
 # python3
+from collections import deque
+import glob
 
 class Edge:
 
@@ -49,6 +51,17 @@ class FlowGraph:
         self.edges[id ^ 1].flow -= flow
 
 
+def read_file_data(filename):
+    with open(filename, 'r') as f:
+        vertex_cnt, edge_cnt = map(int, f.readline().split())
+        graph = FlowGraph(vertex_cnt)
+        for line in f:
+            u, v, capacity = map(int, line.split())
+            graph.add_edge(u - 1, v - 1, capacity)
+    return graph
+
+
+
 def read_data():
     vertex_count, edge_count = map(int, input().split())
     graph = FlowGraph(vertex_count)
@@ -58,9 +71,56 @@ def read_data():
     return graph
 
 
+def get_path(graph, from_, to):
+    """Return an array of edge ids representing the path"""
+    q = deque([from_])
+    trace, visited = {}, set([])
+    while q:
+        u = q.pop()
+        visited.add(u)
+        for edge_id in graph.get_ids(u):
+            e = graph.get_edge(edge_id)
+            if e.v not in visited and e.flow < e.capacity:
+                if e.v == to:
+                    path = [edge_id]
+                    p = u
+                    while p != from_:
+                        p, eid = trace[p]
+                        path.append(eid)
+                    return path[::-1]
+
+                q.appendleft(e.v)
+                trace[e.v] = (u, edge_id)
+    return None
+
+
+def get_min_flow_on_path(graph, path):
+    """Return the minimum flow on the path"""
+    edges = [graph.get_edge(edge_id) for edge_id in path]
+    return min([e.capacity - e.flow for e in edges])
+
+
+def update_flow(graph, edge_id, flow):
+    """add flow to edge edge_id"""
+    graph.add_flow(edge_id, flow)
+    if edge_id & 1 == 0:
+        back_edge = graph.get_edge(edge_id^1)
+        back_edge.capacity = graph.get_edge(edge_id).flow
+
+def calculate_flow(graph, from_):
+    return sum([graph.get_edge(eid).flow for eid in graph.get_ids(from_)])
+
 def max_flow(graph, from_, to):
     flow = 0
     # your code goes here
+    while True:
+        path = get_path(graph, from_, to)
+        if not path:
+            break
+        min_flow = get_min_flow_on_path(graph, path)
+        for e in path:
+            update_flow(graph, e, min_flow)
+    flow = calculate_flow(graph, from_)
     return flow
 
 

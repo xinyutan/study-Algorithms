b69b2c13e7c0e41c1b1e22dd7a703df075410d25
xinyutan <xinyutan@umich.edu>
2020-02-10 07:58:20 -0500

    trie.cpp finished

--- coursera_AlgorithmsOnStrings/Programming-Assignment-1/trie/trie.cpp
+++ coursera_AlgorithmsOnStrings/Programming-Assignment-1/trie/trie.cpp
@@ -11,8 +11,22 @@ typedef map<char, int> edges;
 typedef vector<edges> trie;
 
 trie build_trie(vector<string> & patterns) {
-  trie t;
-  // write your code here
+  trie t(1);
+  int tot_node = 1;
+  for (const auto& p: patterns) {
+    int curr_node = 0;
+    for (int i = 0; i < p.size(); ++i) {
+      if (t[curr_node].find(p[i]) == t[curr_node].end()) {
+        // current_node doesn't contain the p[i]
+        t[curr_node][p[i]] = tot_node;
+        t.push_back({});
+        curr_node = tot_node;
+        tot_node++;
+      }
+      else
+        curr_node = t[curr_node][p[i]];
+    }
+  }
   return t;
 }
 
